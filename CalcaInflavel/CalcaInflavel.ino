bcbbd6c1c44ac511a61a100880b86e1c03d3cbd2
Flaaviaa <48291231+Flaaviaa@users.noreply.github.com>
2021-11-17 21:01:14 -0300

    Add files via upload

---
+++ CalcaInflavel/CalcaInflavel.ino
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define rele_solenoide A0
+
+const int MPU = 0x68;
+
+int AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+void setup() {
+  Serial.begin(9600);
+
+  digitalWrite(rele_solenoide, HIGH);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+}
+
+void loop() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true);
+
+  AcX = Wire.read() << 8 | Wire.read();
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+  Tmp = Wire.read() << 8 | Wire.read();
+  GyX = Wire.read() << 8 | Wire.read();
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53);
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+
+  delay(300);
+}
